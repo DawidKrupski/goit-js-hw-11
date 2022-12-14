75a14457af70a2199277831bd2a97e36ea5c2d23
DawidKrupski <dawid.krupski323@gmail.com>
2022-12-14 20:49:42 +0100

    add files

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import './css/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const input = document.querySelector('input');
 
 const photoPerPage = 40;
-const page = 1;
+let page = 1;
 
-const search = event => {
+const search = async event => {
+  loadMoreBtn.style.display = 'none';
   const searchImage = input.value;
   event.preventDefault();
   const url = `https://pixabay.com/api/?key=13558836-548568db06f41293b437b04a2&q=${searchImage}&image_type=photo$orientation=horizontal$per_page=${photoPerPage}$page=${page}$safesearch=true`;
   const list = document.querySelector('.gallery');
-  const clear = document.querySelector('.gallery>ul');
-  if (clear !== null) {
-    clear.remove();
+  const renderedPhoto = document.querySelector('.gallery>ul');
+  if (renderedPhoto !== null) {
+    page = 1;
+    renderedPhoto.remove();
   }
 
-  axios
+  await axios
     .get(`${url}`)
     .then(function (response) {
       const photoList = document.createElement('ul');
+      photoList.className = 'gallery-list';
       const resultLength = response.data.hits.length;
+      const totalHits = response.data.totalHits;
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
       const photos = response.data.hits;
       list.append(photoList);
       if (resultLength === 0) {
@@ -34,38 +41,118 @@ const search = event => {
         link.className = 'photo-card';
         link.insertAdjacentHTML(
           'beforeend',
-          `<img  src = "${photos[i].webformatURL} loading="lazy"" 
-          alt = "${photos[i].tags}"/> <div class="info"> 
+          `<a> <img  src = "${photos[i].webformatURL} loading="lazy"" 
+          alt = "${photos[i].tags}"/> </a>
+          <div class="info"> 
           <p class="info-item">
-            <b>Likes</b>
-            ${photos[i].likes}
+          <b>Likes</b>
+          ${photos[i].likes}
           </p>
           <p class="info-item">
-            <b>Views</b>
-            ${photos[i].views}
+          <b>Views</b>
+          ${photos[i].views}
           </p>
           <p class="info-item">
             <b>Comments</b>
             ${photos[i].comments}
-          </p>
-          <p class="info-item">
+            </p>
+            <p class="info-item">
             <b>Downloads</b>
             ${photos[i].downloads}
-          </p>
-          </div>`
+            </p>
+            </div>`
         );
+        link.href = `${photos[i].webformatURL}`;
         photoList.append(link);
       }
     })
     .catch(function (error) {
-      // handle error
       console.log(error);
     })
     .finally(function () {
-      // always executed
+      loadMoreBtn.style.display = 'block';
+      refresh();
+    });
+};
+
+const loadMore = event => {
+  page++;
+  const searchImage = input.value;
+  const url = `https://pixabay.com/api/?key=13558836-548568db06f41293b437b04a2&q=${searchImage}&image_type=photo$orientation=horizontal$per_page=${photoPerPage}$page=${page}$safesearch=true`;
+  const renderedPhoto = document.querySelector('.gallery>ul');
+
+  axios
+    .get(`${url}`)
+    .then(function (response) {
+      const resultLength = response.data.hits.length;
+      const photos = response.data.hits;
+      const elementCount = renderedPhoto.childElementCount + photoPerPage;
+      if (resultLength === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      for (let i = 0; i < resultLength; i++) {
+        const link = document.createElement('div');
+        link.className = 'photo-card';
+        link.insertAdjacentHTML(
+          'beforeend',
+          `<a> <img  src = "${photos[i].webformatURL} loading="lazy"" 
+          alt = "${photos[i].tags}"/> </a>
+          <div class="info">
+          <p class="info-item">
+          <b>Likes</b>
+          ${photos[i].likes}
+          </p>
+          <p class="info-item">
+          <b>Views</b>
+          ${photos[i].views}
+            </p>
+            <p class="info-item">
+            <b>Comments</b>
+            ${photos[i].comments}
+          </p>
+          <p class="info-item">
+          <b>Downloads</b>
+          ${photos[i].downloads}
+          </p>
+          </div>`
+        );
+        renderedPhoto.append(link);
+      }
+      console.log(elementCount);
+      if (elementCount >= response.data.totalHits) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+        loadMoreBtn.style.display = 'none';
+        return false;
+      }
+      const { height: cardHeight } = document
+        .querySelector('.gallery-list')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+      refresh();
     });
 };
 
 const btn = document.querySelector('button[type="submit"]');
 btn.addEventListener('click', search);
-// const loadMoreBtn = document.querySelector('button[type="button"]');
+
+const loadMoreBtn = document.querySelector('button[type="button"]');
+loadMoreBtn.style.display = 'none';
+loadMoreBtn.addEventListener('click', loadMore);
+
+let image = new simpleLightbox('.photo-card a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
